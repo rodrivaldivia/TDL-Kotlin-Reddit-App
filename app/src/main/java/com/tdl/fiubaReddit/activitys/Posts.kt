89a417ca700ccfd7c14fc91b0bb61995c0bd9c8f
Gianfranco <giacocipo@gmail.com>
2020-08-04 23:29:44 -0300

    upload activity, work in progress

--- app/src/main/java/com/tdl/fiubaReddit/activitys/Posts.kt
+++ app/src/main/java/com/tdl/fiubaReddit/activitys/Posts.kt
@@ -1,11 +1,13 @@
 package com.tdl.fiubaReddit.activitys
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.tdl.fiubaReddit.post.Post
 import com.tdl.fiubaReddit.R
 import com.tdl.fiubaReddit.postsList.PostsAdapter
@@ -20,9 +22,12 @@ class Posts() : AppCompatActivity() {
         setContentView(R.layout.activity_posts)
         setSupportActionBar(findViewById(R.id.toolbar))
         findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            val intent = Intent(this, NewPost::class.java)
+            ContextCompat.startActivity(this, intent, null)
+
+            /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()*/
         }
         val listView = findViewById<ListView>(R.id.posts_list_view);
         val adapter = PostsAdapter(this, posts)
